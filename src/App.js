fa8d60781f2621088161080362c3b35b784d7f13
hanbyul goo <beammeupplzz@gmail.com>
2022-01-12 17:36:47 +0900

    대화형 리액트 컴포넌트 useState훅까지

--- src/App.js
+++ src/App.js
@@ -20,26 +20,55 @@ const list = [
   }
 ]
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jorden Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
 
+  //대화형 리액트 컴포넌트 : useState Hook
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+  
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text"></input>
+      <input id="search" type="text" onChange={handleChange}></input>
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
 
       <hr />      
 
-      <List/>
-      <List/>
+      <List list={stories} />
     </div>
-  );
-}
+  )
+};
 
-function List() {
-  return list.map((item) => {
-    return (
+
+const List = props => {
+  return props.list.map(item => 
+     (
       <div key={item.objectID}>
         <span>
           <a href="{item.url}">
@@ -50,8 +79,7 @@ function List() {
         <span>{item.num_comments}</span>
         <span>{item.points}</span>
       </div>
-    )
-  })
-}
+  ))};
+
 
 export default App;
